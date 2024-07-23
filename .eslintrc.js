08cb3ff15d5204b981473d3846cdcf1f502af4c3
Nagarjuna <nagarjunaome@gmail.com>
2024-07-23 23:00:57 +0530

    commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,45 +1,40 @@
 module.exports = {
-    parser: 'babel-eslint',
-    extends: [
-      'eslint:recommended',
-      'plugin:react/recommended',
-      'plugin:prettier/recommended'
-    ],
-    plugins: ['prettier'], // Add this line
-    env: {
-      browser: true,
-      es6: true,
-      node: true,
-      jest: true
-    },
-    rules: {
-      // Possible Errors
-      'no-console': 'warn', // Warn on console.log usage
-      'no-debugger': 'error', // Disallow the use of debugger
-  
-      // Best Practices
-      'eqeqeq': ['error', 'always'], // Enforce the use of === and !==
-      'curly': 'error', // Require following curly brace conventions
-  
-      // Variables
-      'no-unused-vars': 'warn', // Warn on unused variables
-      'no-var': 'error', // Require let or const instead of var
-      'prefer-const': 'error', // Prefer const over let when variable is not reassigned
-  
-      // Stylistic Issues
-      'indent': ['error', 2], // Enforce consistent indentation (2 spaces)
-      'quotes': ['error', 'single'], // Enforce single quotes
-      'semi': ['error', 'always'], // Require semicolons
-  
-      // React Specific
-      'react/prop-types': 'off', // Disable prop-types as we are using TypeScript
-      'react/jsx-uses-react': 'off', // React 17+ doesn't need React in scope
-      'react/react-in-jsx-scope': 'off', // React 17+ doesn't need React in scope
-    },
-    settings: {
-      react: {
-        version: 'detect'
-      }
+  parser: '@babel/eslint-parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended'
+  ],
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+    jest: true
+  },
+  parserOptions: {
+    requireConfigFile: false, // This line tells babel-eslint to not require a babel config file
+    babelOptions: {
+      presets: ['@babel/preset-env', '@babel/preset-react']
     }
-  };
-  
+  },
+  rules: {
+    'no-console': 'warn',
+    'no-debugger': 'error',
+    'eqeqeq': ['error', 'always'],
+    'curly': 'error',
+    'no-unused-vars': 'warn',
+    'no-var': 'error',
+    'prefer-const': 'error',
+    'indent': ['error', 2],
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'always'],
+    'react/prop-types': 'off',
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  }
+};
